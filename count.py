c364327b99961faa06389a0e992439d314f2412a
Song Yang <songyang1218@gmail.com>
2020-12-16 18:31:45 -0500

    Copula detection

--- count.py
+++ count.py
@@ -6,29 +6,33 @@ import os
 
 # Counts the json corpus
 
+
 def parse():
-    parser = argparse.ArgumentParser(description="Counts the words in the json corpus")
+    parser = argparse.ArgumentParser(
+        description="Counts the words in the json corpus")
 
     parser.add_argument("directory")
 
     return parser.parse_args()
 
+
 def read_json(path: str, voc: Dict[str, int]):
-    
+
     with open(path, "r") as file:
         article = json.load(file)
 
         for paragraph in article["BodyParagraphs"]:
             words = nltk.tokenize.word_tokenize(paragraph, language="french")
-            
+
             for word in words:
                 if word in voc:
                     voc[word] += 1
                 else:
                     voc[word] = 1
 
+
 def main():
-    
+
     args = parse()
 
     voc = dict()
@@ -43,6 +47,7 @@ def main():
     os.makedirs("data/voc", exist_ok=True)
     with open("data/voc/vocab.voc", 'wb') as file:
         pickle.dump(voc, file)
+
 
 if __name__ == "__main__":
     main()
--- knowledge.py
+++ knowledge.py
@@ -1,9 +1,9 @@
+import os
 import spacy
+import argparse
+import json
 
 # Extract knowledge from dependency parsing
-
-nlp = spacy.load("fr_core_news_sm")
-doc = nlp("Le voisin est docteur")
 
 
 def is_negation(token: spacy.tokens.Token) -> bool:
@@ -13,7 +13,7 @@ def is_negation(token: spacy.tokens.Token) -> bool:
     Limits:
     The method does not look at relative clauses in the predicate.
     """
-    assert(token.dep_ is "nsubj")
+    assert(token.dep_ == "nsubj")
 
     children = set(x.text for x in token.head.children)
     if len(children.intersection(["n'", "ne"])) != 0:
@@ -26,9 +26,34 @@ def is_negation(token: spacy.tokens.Token) -> bool:
     return False
 
 
-for token in doc:
-    if token.dep_ == "nsubj" and token.pos_ != "PRON":
-        if is_negation(token):
-            print(f"{token.text} IS_NOT_A {token.head.text}")
-        else:
-            print(f"{token.text} IS_A {token.head.text}")
+def copula_knowledge(nlp, text):
+    doc = nlp(text, disable=["ner"])
+    for token in doc:
+        if token.dep_ == "nsubj" and token.pos_ != "PRON" and "cop" in [x.dep_ for x in token.head.children]:
+            if is_negation(token):
+                print(f"{token.text} IS_NOT_A {token.head.text}")
+            else:
+                print(f"{token.text} IS_A {token.head.text}")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Extracts copula relationships from corpus directory")
+    parser.add_argument("directory")
+    args = parser.parse_args()
+
+    nlp = spacy.load("fr_core_news_sm")
+    
+    for file in os.listdir(args.directory):
+        path = os.path.join(args.directory, file)
+        
+        with open(path, "r") as file:
+            article = json.load(file)
+
+            for paragraph in article["BodyParagraphs"]:
+
+                copula_knowledge(nlp, paragraph)
+
+
+if __name__ == "__main__":
+    main()
