ec5d722dd1f6cd3ddf16b43d8b1cdccee30527af
Song Yang <songyang1218@gmail.com>
2020-12-17 17:50:59 -0500

    Output prune to file

--- prune.py
+++ prune.py
@@ -6,9 +6,12 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Prune the copula results")
     parser.add_argument("log")
+    parser.add_argument("out")
     args = parser.parse_args()
 
     nlp = fr_core_news_sm.load()
+
+    out = open(args.out, "w", encoding='UTF-8')
 
     with open(args.log, "r", encoding='UTF-8') as file:
         for line in file:
@@ -28,7 +31,8 @@ def main():
             if not (a.pos_ == "NOUN" or a.pos_ == "PROPN"):
                 continue
             
-            print(subject, copula, attribute)
+            print(subject, copula, attribute, file=out)
+    out.close()
 
 if __name__ == "__main__":
     main()
