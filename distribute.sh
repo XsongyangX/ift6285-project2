62055b19540af98d9b2c7bb25cdfd1a612dc5886
Song Yang <songyang1218@gmail.com>
2020-12-16 20:38:38 -0500

    Fix distribution

--- distribute.sh
+++ distribute.sh
@@ -8,10 +8,13 @@ if [ $# -ne 2 ]; then
 fi
 
 corpus=$1
-machineCount=$(($2 - 1))
+if [[ -L $corpus ]]; then
+    corpus=$(readlink -f "$corpus")
+fi
+machineCount=$2
 
 # Create files containing paths
-for i in $(seq 0 $machineCount);
+for i in $(seq 0 $(($machineCount - 1)));
 do
     > "distributed_$i.txt"
 done
@@ -20,14 +23,15 @@ iter=0
 for file in $corpus/*.json;
 do
     section=$(($iter % $machineCount))
+    
+    echo $file >> "distributed_$section.txt"
+    
     iter=$(($iter + 1))
-
-    echo $file >> "distributed_$i.txt"
 done
 
 # Distribute the work over the network
 mkdir -p data/extractions
-for i in $(seq 0 $machineCount);
+for i in $(seq 0 $(($machineCount - 1)));
 do
     pkscreen -S "distributed-$i" ssh -J arcade ens \
         "cd ift6285/project2; python knowledge.py distributed_$i.txt --listed > data/extractions/knowledge_$i.log"
